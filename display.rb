70d68680cfddc4005aea2677f183fa1dadae182c
ratPoopert <50669669+ratPoopert@users.noreply.github.com>
2022-01-24 14:28:23 +0000

    Initial version of Display module

---
+++ display.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Messages for the user to read.
+module Display
+  def title_screen
+    <<~'TITLE_SCREEN'
+      __  __           _                      _           _ 
+      |  \/  |         | |                    (_)         | |
+      | \  / | __ _ ___| |_ ___ _ __ _ __ ___  _ _ __   __| |
+      | |\/| |/ _` / __| __/ _ \ '__| '_ ` _ \| | '_ \ / _` |
+      | |  | | (_| \__ \ ||  __/ |  | | | | | | | | | | (_| |
+      |_|  |_|\__,_|___/\__\___|_|  |_| |_| |_|_|_| |_|\__,_|
+
+                  Press ENTER to continue.
+    TITLE_SCREEN
+  end
+end
